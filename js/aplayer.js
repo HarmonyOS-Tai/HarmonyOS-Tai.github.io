b809f42371b85d3c2e3afde6bc6fef4cbc5b227a
Liu <1832687462@qq.com>
2022-02-05 15:21:22 +0800

    Site updated: 2022-02-05 15:20:54

--- js/aplayer.js
+++ js/aplayer.js
@@ -6,19 +6,100 @@
       new APlayer({
         container: document.getElementById('aplayer'),
         fixed: true,
-        autoplay: true,
+        autoplay: false,
         loop: 'all',
         order: 'random',
-        theme: '#b7daff',
-        preload: 'none',
+        theme: '#36282b',
+        preload: 'auto',
         audio: [
           {
-            name: 'song1',
-            artist: 'artist1',
+            name: '孤勇者',
+            artist: '陈奕迅',
             url: '/music/孤勇者 - 陈奕迅极高音质.mp3',
-            cover: '/img/2021_11_1_2.jpg'
+            cover: '/img/孤勇者封面.jpg'
           }
-        ]
+        ],
+        audio: [
+          {
+            name: '渐渐被你吸引',
+            artist: '坂井泉水',
+            url: '/music/坂井泉水《渐渐被你吸引》.mp3',
+            cover: '/img/坂井泉水.jpg'
+          }
+        ],
+        audio: [
+          {
+            name: 'Good-bye My Loneliness',
+            artist: '坂井泉水',
+            url: '/music/坂井泉水Zard - Good-bye My Loneliness.mp3',
+            cover: '/img/坂井泉水.jpg'
+          }
+        ],
+        audio: [
+          {
+            name: 'Five Hundred Miles',
+            artist: 'Justin Timberlake',
+            url: '/music/Five Hundred Miles - Justin Timberlake、Carey Mulligan、Stark Sands(极高音质).mp3',
+            cover: '/img/Five Hundred Miles.jpg'
+          }
+        ],
+        audio: [
+          {
+            name: '骑在银龙的背上',
+            artist: 'MISIA',
+            url: '/music/骑在银龙的背上 (银の龙の背に乗って) (Live) - MISIA.flac(无损) (1).mp3',
+            cover: '/img/骑在银龙的背上.jpg'
+          }
+        ],
+        audio: [
+          {
+            name: 'Butter-Fly',
+            artist: '和田光司',
+            url: '/music/Butter-Fly - 和田光司(无损）.flac',
+            cover: '/img/Butter-Fly.jpg'
+          }
+        ],
+        audio: [
+          {
+            name: '歌声与微笑',
+            artist: '广播合唱团',
+            url: '/music/中央人民广播电台少年广播合唱团 - 歌声与微笑.mp3',
+            cover: '/img/2021_11_1_15.jpg'
+          }
+        ],
+        audio: [
+          {
+            name: 'Call of Silence',
+            artist: '无',
+            url: '/music/Call of Silence (1).mp3',
+            cover: '/img/Call of Silence.jpg'
+          }
+        ],
+        audio: [
+          {
+            name: '永远同在',
+            artist: '木村弓',
+            url: '/music/いつも何度でも (永远同在) - 木村弓(极高音质).mp3',
+            cover: '/img/永远同在.jpg'
+          }
+        ],
+        audio: [
+          {
+            name: '太阳照常升起',
+            artist: '久石让',
+            url: '/music/太阳照常升起 - 久石让(极高音质).mp3',
+            cover: '/img/太阳照常升起.jpg'
+          }
+        ],
+        audio: [
+          {
+            name: '卡农',
+            artist: '无',
+            url: '/music/卡农.MP3',
+            cover: '/img/卡农.jpg'
+          }
+        ],
       });
     }
   })();
+  
--- js/img-lazyload.js
+++ js/img-lazyload.js
@@ -1,10 +1,21 @@
+// /* global Fluid, CONFIG */
+
+// (function(window, document) {
+//   for (const each of document.querySelectorAll('img[lazyload]')) {
+//     Fluid.utils.waitElementVisible(each, function() {
+//       each.removeAttribute('srcset');
+//       each.removeAttribute('lazyload');
+//     }, CONFIG.lazyload.offset_factor);
+//   }
+// })(window, document);
 /* global Fluid, CONFIG */
 
-(function(window, document) {
+function lazyload(window, document) {
   for (const each of document.querySelectorAll('img[lazyload]')) {
     Fluid.utils.waitElementVisible(each, function() {
       each.removeAttribute('srcset');
       each.removeAttribute('lazyload');
     }, CONFIG.lazyload.offset_factor);
   }
-})(window, document);
+}
+lazyload(window, document);
